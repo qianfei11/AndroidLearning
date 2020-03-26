03a2de5d6446edc853a74eb0caf3e5c60562ddfb
qianfei11 <490584635@qq.com>
2020-03-26 17:07:27 +0800

    2020/3/26

---
+++ Homework04example/app/src/main/java/zjgsu/is/qff/homework04_example/MainActivity.java
@@ -0,0 +1,14 @@
+package zjgsu.is.qff.homework04_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
